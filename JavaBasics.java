32fca38f248e5c62ed7bea5806d38e8335e88644
lokesh976361 <lk.dansroli@gmail.com>
2025-11-14 21:48:40 +0530

    update code

--- JavaBasics.java
+++ JavaBasics.java
@@ -158,5 +158,7 @@ public class JavaBasics{
         public static void main(String args []){
             Scanner sc = new Scanner(System.in);
             String input =sc.next();
+            System.out.println(input);
         }
     } 
+    // kbgkhgfkhfjkgkh
